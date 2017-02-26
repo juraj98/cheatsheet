9f93187922a313c6bf00cef828652711cf01924b
Juraj <juraj982@gmail.com>
2017-02-26 15:23:40 +0100

    H - Menu bugfix

--- js/menus/sideMenusListeners.js
+++ js/menus/sideMenusListeners.js
@@ -1,26 +1,40 @@
 function sideMenusListeners(){
 	$(".homeBtn").on("click", function() {
+		$(".sideMenuItemActive").removeClass("sideMenuItemActive");
+		$(".homeBtn").addClass("sideMenuItemActive");
 		var html = "Home";
 		$(".content").html(html);
 	});
 	$(".messagesBtn").on("click", function() {
+		$(".sideMenuItemActive").removeClass("sideMenuItemActive");
+		$(".messagesBtn").addClass("sideMenuItemActive");
 		var html = "Messages";
 		$(".content").html(html);
 	});
 	$(".remindersBtn").on("click", function() {
+		$(".sideMenuItemActive").removeClass("sideMenuItemActive");
+		$(".remindersBtn").addClass("sideMenuItemActive");
 		remindersClick();
 	});
 	$(".addClass").on("click", function() {
+		$(".sideMenuItemActive").removeClass("sideMenuItemActive");
+		$(".addClass").addClass("sideMenuItemActive");
 		addClassClick();
 	});
 	$(".classBtn").on("click", function() {
+		$(".sideMenuItemActive").removeClass("sideMenuItemActive");
+		$(".classBtn").addClass("sideMenuItemActive");
 		newClassClick($(this).data("classId"));
 
 	});
 	$(".accountBtn").on("click", function() {
+		$(".sideMenuItemActive").removeClass("sideMenuItemActive");
+		$(".accountBtn").addClass("sideMenuItemActive");
 		accountClick();
 	});
 	$(".settingBtn").on("click", function() {
+		$(".sideMenuItemActive").removeClass("sideMenuItemActive");
+		$(".settingBtn").addClass("sideMenuItemActive");
 		var html = "Settings";
 		$(".content").html(html);
 	});
@@ -28,10 +42,5 @@ function sideMenusListeners(){
 		gapi.auth2.getAuthInstance().signOut().then(function() {
 			console.log('User signed out.');
 		});
-	});
-
-	$(".sideMenuItem").on("click", function() {
-		$(".sideMenuItemActive").removeClass("sideMenuItemActive");
-		$(this).addClass("sideMenuItemActive");
 	});
 }
