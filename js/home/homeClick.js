0dfa5b72dec433103dbfd54d6a8a84962b055ffd
Juraj <juraj982@gmail.com>
2017-02-26 15:45:30 +0100

    H - Home responsive

--- js/home/homeClick.js
+++ js/home/homeClick.js
@@ -1,4 +1,4 @@
 function homeClick() {
-	$(".content").html('<div id="hLeftSide"><div class="hHeader">Activity feed:</div></div><div id="hRightSide"><div class="hHeader" id="hRemindersHeader">Reminders:</div></div>');
+	$(".content").html('<div id="hRightSide"><div class="hHeader" id="hRemindersHeader">Reminders:</div></div><div id="hLeftSide"><div class="hHeader">Activity feed:</div></div>');
 	homeInit();
 }
